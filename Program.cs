b23f4be13d1bfcbcf93a5b58aba65f1d8def13d0
Juan Carlos <jcmgx2006@gmail.com>
2026-05-26 08:29:19 -0500

    Inicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n;
+        Console.Write("Cuantas notas desea ingresar: ");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese las notas:" + n);
+    }
+}
