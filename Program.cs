363eb46c03bf30c495de1353c8836d5c1b2cd8b4
Juan Carlos <jcmgx2006@gmail.com>
2026-05-26 08:58:41 -0500

    INGRESO DE NOTAS

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 using System;
+
 class Program
 {
     static void Main()
-    {
+ {
         int n;
+
         Console.Write("Cuantas notas desea ingresar: ");
         n = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Ingrese las notas:" + n);
+        int[] notas = new int[n];
+
+        for (int i = 0; i < n;  i++  )
+    {
+       Console.Write("Ingrese nota " + (i + 1) + ":  ");
+        notas[i] = int.Parse(Console.ReadLine());
     }
+
+   Console.WriteLine("Notas guardadas");
+  }
 }
